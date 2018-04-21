46f8bcc81db54f5eef3451371daaaad8cde4f5c0
student <student@K05-04.lan.itstep.kiev.ua>
2018-04-21 19:47:24 +0300

    LCT Fix

--- TestTimeTable/src/test/java/org/itstep/controller/LessonControllerTest.java
+++ TestTimeTable/src/test/java/org/itstep/controller/LessonControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
@@ -91,7 +92,30 @@ public class LessonControllerTest {
 		Mockito.verify(lessonService, Mockito.times(1)).save(Mockito.any(Lesson.class));
 	}
 	
-	
+	@Test
+	public void testDelete() throws URISyntaxException {
+		Mockito.doNothing().when(lessonService).delete(Mockito.any(Lesson.class));
+		
+		RequestEntity<Lesson> request = new RequestEntity<Lesson>(lessons.get(0), HttpMethod.DELETE, new URI("/lesson"));
+		ResponseEntity response = restTemplate.exchange(request, HttpStatus.class);
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		Mockito.verify(lessonService, Mockito.times(1)).delete(Mockito.any(Lesson.class));
+	}
+
+	@Test
+	public void testGetList() throws URISyntaxException {
+		Mockito.when(lessonService.findAllByStartTime(Mockito.anyLong(), Mockito.anyLong())).thenReturn(lessons);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("start", "44");
+		headers.add("end", "100");
+
+		RequestEntity request = new RequestEntity(headers, HttpMethod.GET, new URI("/lesson/get-all-by-time"));
+		ResponseEntity<List> response = restTemplate.exchange(request, List.class);
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		Mockito.verify(lessonService, Mockito.times(1)).findAllByStartTime(Mockito.anyLong(), Mockito.anyLong());
+	}
 //
 //	@Test
 //	public void testUpdate() {
