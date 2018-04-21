babf713acd041e8f7d998f29e7e26145673b619b
student <student@K05-04.lan.itstep.kiev.ua>
2018-04-21 19:11:39 +0300

    LessonContr

--- TestTimeTable/src/test/java/org/itstep/controller/LessonControllerTest.java
+++ TestTimeTable/src/test/java/org/itstep/controller/LessonControllerTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.itstep.ApplicationRunner;
+import org.itstep.model.Group;
 import org.itstep.model.Lesson;
 import org.itstep.model.Subject;
+import org.itstep.model.Teacher;
 import org.itstep.service.LessonService;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,43 +32,71 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class LessonControllerTest {
+
+	List<Lesson> lessons;
 
 	@Autowired
 	TestRestTemplate restTemplate;
-	
+
 	@MockBean
 	LessonService lessonService;
-	
-	@Ignore
+
+	@Before
+	public void setPreData() {
+
+		Group group = new Group();
+		group.setCourse("TestCourse");
+		group.setName("TestName");
+		group.setSpecialization("TestSpec");
+
+		Subject subject = new Subject();
+		subject.setName("QWAS");
+
+		Teacher teacher = new Teacher();
+
+		teacher.setLogin("TestLogin");
+		teacher.setPassword("TestPassword");
+		teacher.setFirstName("TestFirstName");
+		teacher.setSecondName("TestSecondName");
+		teacher.setSubject(subject);
+
+		for (int i = 1; i <= 3; i++) {
+			Lesson lesson = new Lesson();
+
+			lesson.setCabinet("TestCabinet" + i);
+			lesson.setGroup(group);
+			lesson.setSubject(subject);
+			lesson.setTeacher(teacher);
+			lesson.setStartTime((long) (i*45));
+
+			lessons.add(lesson);
+		}
+
+	}
+
 	@Test
 	public void testSave() throws URISyntaxException {
-		Lesson lesson = new Lesson();
 		
-		Subject subject = new Subject();
-		subject.setName("Java");
-		
-		lesson.setSubject(subject);
+		Mockito.when(lessonService.save(Mockito.any(Lesson.class))).thenReturn(lessons.get(0));
 		
 		
-		Mockito.when(lessonService.save(Mockito.any(Lesson.class))).thenReturn(lesson);
-		RequestEntity<Lesson> request = new RequestEntity<Lesson>(lesson, HttpMethod.POST, new URI("/lesson"));
+		RequestEntity<Lesson> request = new RequestEntity<Lesson>(lessons.get(0), HttpMethod.POST, new URI("/lesson"));
 		ResponseEntity<Lesson> response = restTemplate.exchange(request, Lesson.class);
-		
-		assertEquals(HttpStatus.OK, response.getStatusCode()); 
-		assertEquals("Java", response.getBody().getSubject().getName());
-		
-		Mockito.verify(lessonService , Mockito.times(1)).save(Mockito.any(Lesson.class));
-	}
 
-	
-	@Test
-	public void testUpdate() {
-	}
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals("QWAS", response.getBody().getSubject().getName());
 
-	@Test
-	public void testGetOne() {
+		Mockito.verify(lessonService, Mockito.times(1)).save(Mockito.any(Lesson.class));
 	}
+//
+//	@Test
+//	public void testUpdate() {
+//	}
+//
+//	@Test
+//	public void testGetOne() {
+//	}
 
 }
